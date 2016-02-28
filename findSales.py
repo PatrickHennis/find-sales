2865c752998a239872e07ddc25bc42c0bfec7ad0
Patrick Hennis <patrickhennis@gmail.com>
2016-02-27 19:41:18 -0500

    added comments

--- findSales.py
+++ findSales.py
@@ -5,8 +5,10 @@ line_count = 0
 current_order = 0
 stringOrder = ''
 for line in input_file:
-    for current_order in range (3000000, 4000000):
+    for current_order in range (3071325, 4000000):
         stringOrder = `current_order`
+        # keep 'line' there to go through every zipcode in america or just specify what zipcode you want
+        # i don't recommend keeping line there unless you have a few years
         url = "http://merchnow.com/orders/LookupById?orderId=" + stringOrder + "&postalCode=" + line
         page=urllib2.urlopen(url)
         soup = BeautifulSoup(page.read(), "html.parser")
